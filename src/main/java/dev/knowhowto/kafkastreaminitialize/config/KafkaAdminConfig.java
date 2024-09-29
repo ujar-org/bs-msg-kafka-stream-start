61a7eaf315ae7db10766ef481bba65d93163dedf
Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>
2024-09-29 12:33:15 +0300

    style: fix wrong lexicographical order
    Signed-off-by: Dima Denisenko <1253839+dmitriidenisenko@users.noreply.github.com>

--- src/main/java/dev/knowhowto/kafkastreaminitialize/config/KafkaAdminConfig.java
+++ src/main/java/dev/knowhowto/kafkastreaminitialize/config/KafkaAdminConfig.java
@@ -3,10 +3,10 @@ package dev.knowhowto.kafkastreaminitialize.config;
 import static dev.knowhowto.kafkastreaminitialize.config.Constants.TOPIC_DEFINITION_FOR_INPUT;
 import static dev.knowhowto.kafkastreaminitialize.config.Constants.TOPIC_DEFINITION_FOR_OUTPUT;
 
+import com.iqkv.boot.kafka.config.KafkaTopicDefinitionProperties;
 import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.common.config.TopicConfig;
-import com.iqkv.boot.kafka.config.KafkaTopicDefinitionProperties;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
